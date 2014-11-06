c092c81d56ae6db940bba87445cfafb7b931e730
Will Franzen <wtfranzen@gmail.com>
2014-11-05 18:46:36 -0600

    Done.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var box = document.querySelector('input');
 var btn = document.querySelector('button');
+var out = document.querySelector('p.output');
 
 var main = function() {
     var subreddit = box.value.match(/\/?r?\/?(\w+)/)[1];
@@ -25,10 +26,10 @@ Generator.prototype = {
         var allWords = Object.keys(dictionary);
         var numWords = allWords.length;
 
-        if(!dictionary[last])
-            last = allWords[Math.floor(Math.random()*numWords)];
+        if(!dictionary[this.last])
+            this.last = allWords[Math.floor(Math.random()*numWords)];
 
-        var possibleWords = Object.keys(dictionary[last]);
+        var possibleWords = Object.keys(dictionary[this.last]);
         var numPossibleWords = possibleWords.length;
 
         var word = possibleWords[Math.floor(Math.random()*numPossibleWords)];
@@ -38,12 +39,17 @@ Generator.prototype = {
         return word || '';
     },
     seed: function(str) {
+        var dictionary = this.dictionary;
+
         var words = str.split(/\W+/);
 
         for(var i=0; i<words.length-1; i++) {
             dictionary[words[i]] = dictionary[words[i]] || {};
             dictionary[words[i]][words[i+1]] = dictionary[words[i]][words[i+1]] + 1 || 1;
         }
+    },
+    cut: function() {
+        this.last = '';
     }
 };
 
@@ -82,20 +88,24 @@ var getSentences = function(comments) {
     var allSentences = [];
 
     comments.forEach(function(comment) {
-        comment.match(/^\\s+[A-Za-z,;'\"\\s]+[.?!]$/g/**thaks stackoverflow!**/).forEach(function(sentence) {
-            allSentences.push(sentence);
+        comment.split(/[.?!]/).forEach(function(sentence) {
+            if(sentence)
+                allSentences.push(sentence);
         });
     });
 
     allSentences = allSentences.map(clean);
 
-    return sentences;
+    return allSentences;
 };
 
 var clean = function(sentence) {
     sentence = sentence.trim();
     sentence = sentence.toLowerCase();
     sentence = sentence.replace(/[^\w\s]/g, '');
+
+    sentence = sentence.replace('i ', 'I '); // Little hack
+
     return sentence;
 };
 
@@ -105,27 +115,44 @@ var generateIpsum = function(res) {
 
     var generator = new Generator();
 
+    window.gen = generator;
+
     sentences.forEach(function(sentence) {
         generator.seed(sentence);
     });
 
     var ipsum = "";
-    for(var i=0; i<300; i++)
-        ipsum =+ generateSentence(generator);
-    return ipsum;
+    for(var i=0; i<6; i++)
+        ipsum += generateParagraph(generator) + "<br><br>";
+
+    output(ipsum);
 };
 
 var generateSentence = function(generator) {
-    var length = Math.floor(Math.random() * 5) + 3;
+    var length = Math.floor(Math.random() * 8) + 3;
 
     var sentence = "";
 
     for(var i=0; i<length; i++)
-        sentence += generator.next();
+        sentence += generator.next() + " ";
+
+    sentence = sentence.trim();
     sentence += randomPunctuation();
-    sentence[0] = sentence[0].toUpperCase();
+    sentence = sentence[0].toUpperCase() + sentence.substr(1);
+
+    generator.cut();
 
     return sentence;
+};
+
+var generateParagraph = function(generator) {
+    var paragraph = "";
+    var length = Math.floor(Math.random()*20)+4;
+
+    for(var i=0;i<length;i++)
+        paragraph += generateSentence(generator) + " ";
+
+    return paragraph;
 };
 
 var randomPunctuation = function() {
@@ -133,9 +160,7 @@ var randomPunctuation = function() {
 };
 
 var output = function(str) {
-    var out = document.createElement('textarea');
-    out.innerText = str;
-    document.body.appendChild(out);
+    out.innerHTML = str;
 };
 
 btn.addEventListener('click', main);
